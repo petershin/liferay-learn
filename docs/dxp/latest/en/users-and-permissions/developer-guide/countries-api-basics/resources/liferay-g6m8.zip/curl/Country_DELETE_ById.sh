38cf20662f315bcbb82db447fbe24ea7f8db96d4
mtokudome <makoto.tokudome@liferay.com>
2022-06-03 10:44:20 -0700

    LRDOCS-10538 cURL resources

---
+++ docs/dxp/latest/en/users-and-permissions/developer-guide/countries-api-basics/resources/liferay-g6m8.zip/curl/Country_DELETE_ById.sh
@@ -0,0 +1,4 @@
+curl \
+	-X DELETE \
+	"http://localhost:8080/o/headless-admin-address/v1.0/countries/${1}" \
+	-u "test@liferay.com:learn"
---
+++ docs/dxp/latest/en/users-and-permissions/developer-guide/regions-api-basics/resources/liferay-r2p3.zip/curl/Region_POST_ToCountry.sh
@@ -0,0 +1,6 @@
+curl \
+	-H "Content-Type: application/json" \
+	-X POST \
+	"http://localhost:8080/o/headless-admin-address/v1.0/countries/${1}/regions" \
+	-d "{\"name\": \"Foo\", \"regionCode\": \"ABL\"}" \
+	-u "test@liferay.com:learn"

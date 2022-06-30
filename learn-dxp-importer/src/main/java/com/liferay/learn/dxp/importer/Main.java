a01c0174b0f1f4a081a48493631aa5f90418e98d
Brian Chan <brian.chan@liferay.com>
2022-06-29 20:22:29 -0700

    LRDOCS-10618 SF

--- learn-dxp-importer/src/main/java/com/liferay/learn/dxp/importer/Main.java
+++ learn-dxp-importer/src/main/java/com/liferay/learn/dxp/importer/Main.java
@@ -63,6 +63,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
@@ -412,11 +413,7 @@ public class Main {
 				}
 			}
 
-			if (!admonitionLineSB.toString(
-				).equals(
-					""
-				)) {
-
+			if (!Objects.equals(admonitionLineSB.toString(), "")) {
 				line = admonitionLineSB.toString();
 			}
 

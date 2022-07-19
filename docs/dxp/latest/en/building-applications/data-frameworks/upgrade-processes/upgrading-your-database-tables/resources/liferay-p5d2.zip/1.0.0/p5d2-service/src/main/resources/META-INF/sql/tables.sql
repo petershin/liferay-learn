d8021e03071192b104d0c3b37f8909d81e8c6f47
mtokudome <makoto.tokudome@liferay.com>
2022-07-19 12:09:40 -0700

    LRDOCS-10461 liferay-p5d2.zip files

--- docs/dxp/latest/en/building-applications/data-frameworks/upgrade-processes/upgrading-your-database-tables/resources/liferay-p5d2.zip/1.0.0/p5d2-service/src/main/resources/META-INF/sql/tables.sql
+++
@@ -1,6 +0,0 @@
-create table P5D2_P5D2Entry (
-	p5d2EntryId LONG not null primary key,
-	able LONG,
-	baker BOOLEAN,
-	foo VARCHAR(75) null
-);
--- docs/dxp/latest/en/building-applications/data-frameworks/upgrade-processes/upgrading-your-database-tables/resources/liferay-p5d2.zip/2.0.0/p5d2-service/src/main/resources/META-INF/sql/tables.sql
+++
@@ -1,6 +0,0 @@
-create table P5D2_P5D2Entry (
-	p5d2EntryId LONG not null primary key,
-	able DATE null,
-	bar VARCHAR(75) null,
-	charlie VARCHAR(75) null
-);

6704ce2706d65aa8a78734d5e917f9d2cd101934
mtokudome <makoto.tokudome@liferay.com>
2022-10-10 14:42:53 -0700

    LRDOCS-10060 add ImportTaskContent

--- docs/dxp/latest/en/headless-delivery/consuming-apis/batch-engine-api-basics/resources/liferay-g4j2.zip/java/ImportTaskContent_GET_ById.java
+++ docs/dxp/latest/en/headless-delivery/consuming-apis/batch-engine-api-basics/resources/liferay-g4j2.zip/java/ImportTaskContent_GET_ById.java
@@ -1,19 +1,19 @@
 import com.liferay.headless.batch.engine.client.http.HttpInvoker;
-import com.liferay.headless.batch.engine.client.resource.v1_0.ExportTaskResource;
+import com.liferay.headless.batch.engine.client.resource.v1_0.ImportTaskResource;
 
 import java.io.FileOutputStream;
 
-public class ExportTaskContent_GET_ById {
+public class ImportTaskContent_GET_ById {
 
 	public static void main(String[] args) throws Exception {
-		ExportTaskResource.Builder builder = ExportTaskResource.builder();
+		ImportTaskResource.Builder builder = ImportTaskResource.builder();
 
-		ExportTaskResource exportTaskResource = builder.authentication(
+		ImportTaskResource importTaskResource = builder.authentication(
 			"test@liferay.com", "learn"
 		).build();
 
 		HttpInvoker.HttpResponse httpResponse =
-			exportTaskResource.getExportTaskContentHttpResponse(
+			importTaskResource.getImportTaskContentHttpResponse(
 				Long.valueOf(args[0]));
 
 		try (FileOutputStream fileOutputStream = new FileOutputStream(

d0c9ed827ee87a55343273a4f74e880ffe9a2936
Aswin Rajeev <aswin.rajeev@liferay.com>
2022-10-12 15:28:10 +0100

    LRDOCS-10060 Added missing quotes

--- docs/dxp/latest/en/headless-delivery/consuming-apis/batch-engine-api-basics/resources/liferay-g4j2.zip/java/ImportTask_DELETE_ById.java
+++ docs/dxp/latest/en/headless-delivery/consuming-apis/batch-engine-api-basics/resources/liferay-g4j2.zip/java/ImportTask_DELETE_ById.java
@@ -3,7 +3,7 @@ import com.liferay.headless.batch.engine.client.resource.v1_0.ImportTaskResource
 public class ImportTask_DELETE_ById {
 
 	/**
-	 * java -classpath .:* -DclassName=able -Ddata=baker ImportTask_DELETE_ById
+	 * java -classpath .:* -DclassName=able -Ddata="baker" ImportTask_DELETE_ById
 	 */
 	public static void main(String[] args) throws Exception {
 		ImportTaskResource.Builder builder = ImportTaskResource.builder();

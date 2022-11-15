dbb2e6fc91d5b2d3cf16f88aa4756aa6284d5b4c
mtokudome <makoto.tokudome@liferay.com>
2022-11-15 10:38:56 -0800

    LRDOCS-10339 fix redirectUri

--- docs/dxp/latest/en/headless-delivery/using-oauth2/securing-your-app-with-oauth2/resources/liferay-c2b6-overlay/src/grant-type-authorization-code/utils/config.js
+++ docs/dxp/latest/en/headless-delivery/using-oauth2/securing-your-app-with-oauth2/resources/liferay-c2b6-overlay/src/grant-type-authorization-code/utils/config.js
@@ -1,3 +1,3 @@
 export const clientId = '';
 export const clientSecret = '';
-export const redirectUri = 'http://localhost:3000';
+export const redirectUri = 'http://localhost:3000/grant-type-authorization-code';

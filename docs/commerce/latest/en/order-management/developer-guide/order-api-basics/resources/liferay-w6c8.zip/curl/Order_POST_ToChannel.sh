368b09394e92200f74debd3017ef9ea736c363ca
Aswin Rajeev <aswin.rajeev@liferay.com>
2022-11-25 13:04:09 +0000

    LRDOCS-11142 minor typo correction and SF

--- docs/commerce/latest/en/order-management/developer-guide/order-api-basics/resources/liferay-w6c8.zip/curl/Order_POST_ToChannel.sh
+++ docs/commerce/latest/en/order-management/developer-guide/order-api-basics/resources/liferay-w6c8.zip/curl/Order_POST_ToChannel.sh
@@ -2,5 +2,5 @@ curl \
 	 -H "Content-Type: application/json" \
 	 -X POST \
 	 "http://localhost:8080/o/headless-commerce-admin-order/v1.0/orders" \
-	 -d "{\"accountId\": $1, \"channelId\": ${2}, \"currencyCode\": \"${3}\"}" \
+	 -d "{\"accountId\": ${1}, \"channelId\": ${2}, \"currencyCode\": \"${3}\"}" \
 	 -u "test@liferay.com:learn"

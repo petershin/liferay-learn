732a02305bff83481643adfb405d322642c4f629
Gabriel Prates <gabsprates@gmail.com>
2022-12-02 05:49:48 -0300

    Make setup_tutorial.sh find packages with npm tool

--- docs/_template/js/setup_tutorial.sh
+++ docs/_template/js/setup_tutorial.sh
@@ -1,47 +1,36 @@
 #!/bin/sh
 
 function main {
-	if [ -z "${NPM_CONFIG_PREFIX}" ]
+	echo "Checking dependencies..."
+
+	local tmp_packages_file="$(mktemp -d)/packages"
+
+	local necessay_packages=(generator-liferay-fragments generator-liferay-js generator-liferay-theme yarn yo)
+	local missing_packages=()
+
+	npm list --depth=0 -g ${necessay_packages[@]} > $tmp_packages_file
+
+	for index in ${!necessay_packages[@]}; do
+		package=${necessay_packages[$index]}
+	
+		grep " ${package}@" $tmp_packages_file
+
+		if [ $? -gt 0 ]
+		then
+			missing_packages+=( $package )
+		fi
+	done
+
+	rm $tmp_packages_file
+	echo ""
+
+	if [ ${#missing_packages[@]} -gt 0 ]
 	then
-		echo "The environment variable NPM_CONFIG_PREFIX is not set and needs to point to a folder for storing Node packages. Run this command:
+		echo "Some tutorial dependencies are missing. Run this command:"
+		echo ""
+		echo "	npm install -g ${missing_packages[@]}"
 
-		export NPM_CONFIG_PREFIX=~/.npm-global"
-
-		exit
-	fi
-
-    local export_path_command="export PATH=\${PATH}:\${NPM_CONFIG_PREFIX}/bin"
-    local npm_bin_dir_name="${NPM_CONFIG_PREFIX}"/bin
-    local npm_modules_dir_name="${NPM_CONFIG_PREFIX}"/lib/node_modules
-
-    if [[ "${OSTYPE}" == "cygwin" ]] &&
-       [[ "${OSTYPE}" == "msys" ]]
-    then
-        export_path_command="export PATH=\${PATH}:\${NPM_CONFIG_PREFIX}"
-        npm_bin_dir_name="${NPM_CONFIG_PREFIX}"
-        npm_modules_dir_name="${NPM_CONFIG_PREFIX}"/node_modules
-    fi
-
-	if [[ "${PATH}" != *"${npm_bin_dir_name}"* ]]
-	then
-		echo "The environment variable PATH does not include ${npm_bin_dir_name}. Add it to your PATH. Run this command:
-
-		${export_path_command}"
-
-		exit
-	fi
-
-	if [ ! -d "${npm_modules_dir_name}/generator-liferay-fragments" ] ||
-	   [ ! -d "${npm_modules_dir_name}/generator-liferay-js" ] ||
-	   [ ! -d "${npm_modules_dir_name}/generator-liferay-theme" ] ||
-	   [ ! -d "${npm_modules_dir_name}/yarn" ] ||
-	   [ ! -d "${npm_modules_dir_name}/yo" ]
-	then
-		echo "A tutorial dependency is missing. Run this command:
-
-		npm install -g generator-liferay-fragments generator-liferay-js generator-liferay-theme yarn yo"
-
-		exit
+		exit 1;
 	fi
 
 	echo "Your environment is ready for the tutorial."

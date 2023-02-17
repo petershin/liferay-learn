7a9762cff01244cc6e1e63ed2db8592dc3b35eb3
Russell Bohl <russell.bohl@liferay.com>
2023-02-17 13:52:30 -0500

    LRDOCS-7800 temp fix to null permission checker, investigating better solutions

--- docs/dxp/latest/en/using-search/developer-guide/search-queries-and-filters/resources/liferay-b9f3.zip/b9f3-ptype/src/main/java/com/acme/b9f3/osgi/commands/B9F3OSGiCommands.java
+++ docs/dxp/latest/en/using-search/developer-guide/search-queries-and-filters/resources/liferay-b9f3.zip/b9f3-ptype/src/main/java/com/acme/b9f3/osgi/commands/B9F3OSGiCommands.java
@@ -1,15 +1,16 @@
 package com.acme.b9f3.osgi.commands;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.search.document.Document;
@@ -25,12 +26,19 @@ import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.searcher.SearchResponse;
 import com.liferay.portal.search.searcher.Searcher;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 @Component(
-		property = {"osgi.command.function=search", "osgi.command.scope=b9f3"},
-		service = B9F3OSGiCommands.class
-	)
+	property = {"osgi.command.function=search", "osgi.command.scope=b9f3"},
+	service = B9F3OSGiCommands.class
+)
 public class B9F3OSGiCommands {
-	public void search(String keywords) {
+
+	public void search(String keywords) throws PortalException {
 		MatchQuery titleQuery = _queries.match(
 			StringBundler.concat(
 				"localized_", Field.TITLE, StringPool.UNDERLINE, LocaleUtil.US),
@@ -45,8 +53,18 @@ public class B9F3OSGiCommands {
 		SearchRequestBuilder searchRequestBuilder =
 			_searchRequestBuilderFactory.builder();
 
-		//long companyId = _portal.getDefaultCompanyId();
-		PermissionChecker permissionChecker = PermissionThreadLocal.getPermissionChecker();
+		Role adminRole = RoleLocalServiceUtil.getRole(
+			_portal.getDefaultCompanyId(), "Administrator");
+
+		List<User> adminUsers = _userLocalService.getRoleUsers(
+			adminRole.getRoleId());
+
+		PermissionChecker permissionChecker =
+			PermissionCheckerFactoryUtil.create(
+				_userLocalService.getUser(adminUsers.get(0).getUserId()));
+
+		PermissionThreadLocal.setPermissionChecker(permissionChecker);
+
 		searchRequestBuilder.withSearchContext(
 			searchContext -> {
 				searchContext.setCompanyId(permissionChecker.getCompanyId());
@@ -91,5 +109,8 @@ public class B9F3OSGiCommands {
 
 	@Reference
 	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }

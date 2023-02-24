05c3c6bb59d1707b00dda410c0d9a52241897b70
Aswin Rajeev <aswin.rajeev@liferay.com>
2023-02-24 15:46:35 +0000

    LRDOCS-11597 curl examples

---
+++ docs/commerce/latest/en/pricing/developer-guide/discount-api-basics/resources/liferay-r6f7.zip/curl/Discount_POST_ToCatalog.sh
@@ -0,0 +1,6 @@
+curl \
+	 -H "Content-Type: application/json" \
+	 -X POST \
+	 "http://localhost:8080/o/headless-commerce-admin-pricing/v2.0/price-lists" \
+	 -d "{\"level\": \"L1\", \"limitationType\": \"unlimited\", \"target\": \"products\", \"title\": \"Foo\", \"usePercentage\": true}" \
+	 -u "test@liferay.com:learn"

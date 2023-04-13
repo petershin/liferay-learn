d550640386dcb7bd3b6954c0ba5877089503c040
JamesG <james.garcia@liferay.com>
2023-04-13 16:25:26 -0700

    LRDOCS-11556 adds entry ERCs to POST curl scripts

--- docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/using-nested-fields-with-rest-apis/resources/liferay-w4s7.zip/curl/Able_POST_ToCompany.sh
+++ docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/using-nested-fields-with-rest-apis/resources/liferay-w4s7.zip/curl/Able_POST_ToCompany.sh
@@ -2,19 +2,19 @@ curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/c/ables/" \
-	-d "{\"name\": \"Able 1\"}" \
+	-d "{\"name\": \"Able 1\", \"externalReferenceCode\": \"able-one\"}" \
 	-u "test@liferay.com:learn"
 
 curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/c/ables/" \
-	-d "{\"name\": \"Able 2\"}" \
+	-d "{\"name\": \"Able 2\", \"externalReferenceCode\": \"able-two\"}" \
 	-u "test@liferay.com:learn"
 
 curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/c/ables/" \
-	-d "{\"name\": \"Able 3\"}" \
+	-d "{\"name\": \"Able 3\", \"externalReferenceCode\": \"able-three\"}" \
 	-u "test@liferay.com:learn"
--- docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/using-nested-fields-with-rest-apis/resources/liferay-w4s7.zip/curl/Baker_POST_ToCompany.sh
+++ docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/using-nested-fields-with-rest-apis/resources/liferay-w4s7.zip/curl/Baker_POST_ToCompany.sh
@@ -2,19 +2,19 @@ curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/c/bakers/" \
-	-d "{\"name\": \"Baker 1\", \"r_ableToBaker_c_ableId\": ${1}}" \
+	-d "{\"name\": \"Baker 1\", \"externalReferenceCode\": \"baker-one\", \"r_ableToBaker_c_ableId\": ${1}}" \
 	-u "test@liferay.com:learn"
 
 curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/c/bakers/" \
-	-d "{\"name\": \"Baker 2\", \"r_ableToBaker_c_ableId\": ${1}}" \
+	-d "{\"name\": \"Baker 2\", \"externalReferenceCode\": \"baker-two\", \"r_ableToBaker_c_ableId\": ${1}}" \
 	-u "test@liferay.com:learn"
 
 curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/c/bakers/" \
-	-d "{\"name\": \"Baker 3\", \"r_ableToBaker_c_ableId\": ${1}}" \
+	-d "{\"name\": \"Baker 3\", \"externalReferenceCode\": \"baker-three\", \"r_ableToBaker_c_ableId\": ${1}}" \
 	-u "test@liferay.com:learn"

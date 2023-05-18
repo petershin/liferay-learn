51e5e7fe6fd8aa80c4c260d7fa5453505a256cb2
Alec Shay <Alec.Shay@liferay.com>
2023-05-18 12:37:09 -0700

    LRDOCS-11827 Remove code from exercises for separate review

--- docs/courses/latest/en/cloud-administrator/liferay-experience-cloud-self-managed/cloud-development-lifecycle/configuring-liferay-for-a-custom-feedback-counter-service/resources/liferay-w9g6.zip/src/main/resources/static/oauth2.js
+++
@@ -1,9 +0,0 @@
-async function fetchFeedback() {
-  const resp = await fetch('/api/helpfulfeedback');
-
-  document.getElementById("helpful").innerHTML = await resp.text();
-
-  const resp2 = await fetch('/api/nothelpfulfeedback');
-
-  document.getElementById("not helpful").innerHTML = await resp2.text();
-}

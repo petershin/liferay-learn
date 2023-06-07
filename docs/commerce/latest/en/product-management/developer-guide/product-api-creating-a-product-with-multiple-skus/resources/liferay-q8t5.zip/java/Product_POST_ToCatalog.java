62d8249e696ee05b91a95fa92fb495788ebc1fdf
Aswin Rajeev <aswin.rajeev@liferay.com>
2023-06-07 15:14:41 +0100

    LRDOCS-11787 java example

---
+++ docs/commerce/latest/en/product-management/developer-guide/product-api-creating-a-product-with-multiple-skus/resources/liferay-q8t5.zip/java/Product_POST_ToCatalog.java
@@ -0,0 +1,88 @@
+import com.liferay.headless.commerce.admin.catalog.client.dto.v1_0.Product;
+import com.liferay.headless.commerce.admin.catalog.client.dto.v1_0.ProductOption;
+import com.liferay.headless.commerce.admin.catalog.client.dto.v1_0.Sku;
+import com.liferay.headless.commerce.admin.catalog.client.dto.v1_0.SkuOption;
+import com.liferay.headless.commerce.admin.catalog.client.resource.v1_0.ProductResource;
+
+import java.util.HashMap;
+
+public class Product_POST_ToCatalog {
+
+	/**
+	 * java -classpath .:* -DcatalogId=1234 -DoptionId=5678 Product_POST_ToCatalog
+	 */
+	public static void main(String[] args) throws Exception {
+		ProductResource.Builder builder = ProductResource.builder();
+
+		ProductResource productResource = builder.authentication(
+			"test@liferay.com", "learn"
+		).build();
+
+		System.out.println(
+			productResource.postProduct(
+				new Product() {
+					{
+						active = true;
+						catalogId = Long.valueOf(
+							System.getProperty("catalogId"));
+
+						name = new HashMap<String, String>() {
+							{
+								put("en_US", "Foo");
+							}
+						};
+						productOptions = new ProductOption[] { new ProductOption() {
+								{
+									fieldType = "select";
+									key = "foo";
+
+									name = new HashMap<String, String>() {
+										{
+											put("en_US", "Foo");
+										}
+									};
+									optionId = Long.valueOf(
+										System.getProperty("optionId"));
+									required = true;
+									skuContributor = true;
+								}
+							}
+						};
+
+						productType = "simple";
+						skus = new Sku[] { new Sku() {
+								{
+									published = true;
+									purchasable = true;
+									sku = "TEST001";
+									skuOptions = new SkuOption[] {
+										new SkuOption() {
+											{
+												key = "foo";
+												value = "Able";
+											}
+										}
+									};
+								}
+							},
+							new Sku() {
+								{
+									published = true;
+									purchasable = true;
+									sku = "TEST002";
+									skuOptions = new SkuOption[] {
+										new SkuOption() {
+											{
+												key = "foo";
+												value = "Baker";
+											}
+										}
+									};
+								}
+							}
+						};
+					}
+				}));
+	}
+
+}

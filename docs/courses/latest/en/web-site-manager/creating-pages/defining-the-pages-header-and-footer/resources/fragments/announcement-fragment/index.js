81f408dc7266cb1746f498f3ec85f83ae3a7bced
JamesG <james.garcia@liferay.com>
2023-10-11 11:28:43 -0700

    LRDOCS-12131 condenses announcement JS per Julien

--- docs/courses/latest/en/web-site-manager/creating-pages/defining-the-pages-header-and-footer/resources/fragments/announcement-fragment/index.js
+++ docs/courses/latest/en/web-site-manager/creating-pages/defining-the-pages-header-and-footer/resources/fragments/announcement-fragment/index.js
@@ -1,30 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    handleAnnouncementBanner();
-});
+const announcementBanner = document.getElementById("announcement");
+const closeButton = document.getElementById("closeAnnouncement");
 
-Liferay.on('endNavigate', function() {
-    handleAnnouncementBanner();
-});
-
-function handleAnnouncementBanner() {
-    const announcementBanner = document.getElementById("announcement");
-    const closeButton = document.getElementById("closeAnnouncement");
-
-    if (!announcementBanner || !closeButton) {
-        return;
-    }
-
-    if (localStorage.getItem("announcementClosed") !== "true") {
-        announcementBanner.classList.remove("announcement-banner-hidden");
-        announcementBanner.classList.add("announcement-banner-visible");
-    } else {
-        announcementBanner.classList.remove("announcement-banner-visible");
-        announcementBanner.classList.add("announcement-banner-hidden");
-    }
-
-    closeButton.addEventListener("click", function() {
-        announcementBanner.classList.remove("announcement-banner-visible");
-        announcementBanner.classList.add("announcement-banner-hidden");
-        localStorage.setItem("announcementClosed", "true");
-    });
+if (localStorage.getItem("announcementClosed") !== "true") {
+    announcementBanner.classList.remove("hide");
 }
+
+closeButton.addEventListener("click", function() {
+    announcementBanner.classList.add("hide");
+    localStorage.setItem("announcementClosed", "true");
+});

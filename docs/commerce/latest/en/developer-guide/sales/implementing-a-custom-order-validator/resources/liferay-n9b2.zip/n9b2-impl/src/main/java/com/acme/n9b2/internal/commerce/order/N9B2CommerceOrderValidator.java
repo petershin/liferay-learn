8df3e903cb0e31068e49e2d61f4bbaf766712aba
Aswin Rajeev <aswin.rajeev@liferay.com>
2023-11-29 12:30:06 +0000

    LRDOCS-12449 Custom Order Validator Compile Error

--- docs/commerce/latest/en/developer-guide/sales/implementing-a-custom-order-validator/resources/liferay-n9b2.zip/n9b2-impl/src/main/java/com/acme/n9b2/internal/commerce/order/N9B2CommerceOrderValidator.java
+++ docs/commerce/latest/en/developer-guide/sales/implementing-a-custom-order-validator/resources/liferay-n9b2.zip/n9b2-impl/src/main/java/com/acme/n9b2/internal/commerce/order/N9B2CommerceOrderValidator.java
@@ -7,6 +7,7 @@ import com.liferay.commerce.order.CommerceOrderValidatorResult;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.BigDecimalUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
 import java.math.BigDecimal;
@@ -33,7 +34,7 @@ public class N9B2CommerceOrderValidator implements CommerceOrderValidator {
 	@Override
 	public CommerceOrderValidatorResult validate(
 			Locale locale, CommerceOrder commerceOrder, CPInstance cpInstance,
-			int quantity)
+			BigDecimal quantity)
 		throws PortalException {
 
 		if (cpInstance == null) {
@@ -43,7 +44,7 @@ public class N9B2CommerceOrderValidator implements CommerceOrderValidator {
 		BigDecimal price = cpInstance.getPrice();
 
 		if ((price.doubleValue() > _MAX_ITEM_PRICE) &&
-			(quantity > _MAX_ITEM_QUANTITY)) {
+			BigDecimalUtil.gt(quantity, _MAX_ITEM_QUANTITY)) {
 
 			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 				"content.Language", locale, getClass());
@@ -53,7 +54,7 @@ public class N9B2CommerceOrderValidator implements CommerceOrderValidator {
 				LanguageUtil.format(
 					resourceBundle,
 					"this-expensive-item-has-a-maximum-quantity-of-x",
-					Integer.valueOf(_MAX_ITEM_QUANTITY)));
+					_MAX_ITEM_QUANTITY.toString()));
 		}
 
 		return new CommerceOrderValidatorResult(true);
@@ -67,7 +68,8 @@ public class N9B2CommerceOrderValidator implements CommerceOrderValidator {
 		BigDecimal price = commerceOrderItem.getUnitPrice();
 
 		if ((price.doubleValue() > _MAX_ITEM_PRICE) &&
-			(commerceOrderItem.getQuantity() > _MAX_ITEM_QUANTITY)) {
+			BigDecimalUtil.gt(
+				commerceOrderItem.getQuantity(), _MAX_ITEM_QUANTITY)) {
 
 			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 				"content.Language", locale, getClass());
@@ -77,7 +79,7 @@ public class N9B2CommerceOrderValidator implements CommerceOrderValidator {
 				LanguageUtil.format(
 					resourceBundle,
 					"expensive-items-have-a-maximum-order-quantity-of-x",
-					Integer.valueOf(_MAX_ITEM_QUANTITY)));
+					_MAX_ITEM_QUANTITY.toString()));
 		}
 
 		return new CommerceOrderValidatorResult(true);
@@ -85,6 +87,6 @@ public class N9B2CommerceOrderValidator implements CommerceOrderValidator {
 
 	private static final double _MAX_ITEM_PRICE = 100.0;
 
-	private static final int _MAX_ITEM_QUANTITY = 10;
+	private static final BigDecimal _MAX_ITEM_QUANTITY = new BigDecimal(10);
 
 }

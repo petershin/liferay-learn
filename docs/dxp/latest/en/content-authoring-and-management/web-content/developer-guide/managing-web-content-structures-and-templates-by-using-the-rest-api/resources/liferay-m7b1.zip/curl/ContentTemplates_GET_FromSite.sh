444e1671fcb4a148fba007b4baa9ec6a4d4bbd4b
Eric Moritsuka <ericthmoritsuka@gmail.com>
2024-02-01 06:49:54 -0300

    LRDOCS-12645 formatted .sh files

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/managing-web-content-structures-and-templates-by-using-the-rest-api/resources/liferay-m7b1.zip/curl/ContentTemplates_GET_FromSite.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/managing-web-content-structures-and-templates-by-using-the-rest-api/resources/liferay-m7b1.zip/curl/ContentTemplates_GET_FromSite.sh
@@ -1,2 +1,3 @@
-curl "http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/content-templates" \
-		-u "test@liferay.com:learn"
+curl \
+	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/content-templates" \
+	-u "test@liferay.com:learn"

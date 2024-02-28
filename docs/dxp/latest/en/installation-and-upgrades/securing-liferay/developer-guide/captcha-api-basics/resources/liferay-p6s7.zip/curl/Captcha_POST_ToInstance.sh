08d8bf346445340b40b4fee7399350a6ba2056f4
Aswin Rajeev <aswin.rajeev@liferay.com>
2024-02-28 13:25:14 +0000

    LRDOCS-12534 corrected quote usage

--- docs/dxp/latest/en/installation-and-upgrades/securing-liferay/developer-guide/captcha-api-basics/resources/liferay-p6s7.zip/curl/Captcha_POST_ToInstance.sh
+++ docs/dxp/latest/en/installation-and-upgrades/securing-liferay/developer-guide/captcha-api-basics/resources/liferay-p6s7.zip/curl/Captcha_POST_ToInstance.sh
@@ -2,8 +2,8 @@ curl \
    "http://localhost:8080/o/captcha/v1.0/captcha/response" \
    --data-raw '
       {
-         "answer": "'"${1}"'",
-         "token": "'"${2}"'"
+         "answer": "'${1}'",
+         "token": "'${2}'"
       }' \
    --header "Content-Type: application/json" \
    --request "POST" \

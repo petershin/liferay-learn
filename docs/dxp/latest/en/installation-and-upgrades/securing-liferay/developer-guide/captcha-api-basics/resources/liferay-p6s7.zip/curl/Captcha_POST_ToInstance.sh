ff741a82e7a1fdef15c8fe2e2ec090fa4a905636
Aswin Rajeev <aswin.rajeev@liferay.com>
2024-02-28 09:30:49 +0000

    LRDOCS-12534 escape single quote with double quote

--- docs/dxp/latest/en/installation-and-upgrades/securing-liferay/developer-guide/captcha-api-basics/resources/liferay-p6s7.zip/curl/Captcha_POST_ToInstance.sh
+++ docs/dxp/latest/en/installation-and-upgrades/securing-liferay/developer-guide/captcha-api-basics/resources/liferay-p6s7.zip/curl/Captcha_POST_ToInstance.sh
@@ -1,10 +1,10 @@
 curl \
    "http://localhost:8080/o/captcha/v1.0/captcha/response" \
-   --data-raw "
+   --data-raw '
       {
-         \"answer\": \"${1}\",
-         \"token\": \"${2}\"
-      }" \
+         "answer": "'"${1}"'",
+         "token": "'"${2}"'"
+      }' \
    --header "Content-Type: application/json" \
    --request "POST" \
-   --user "test@liferay.com:learn"
+   --user "test@liferay.com:learn" \

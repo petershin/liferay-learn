75221cac9a8f3c9b08671ce6bf86aa097cbedde1
Aswin Rajeev <aswin.rajeev@liferay.com>
2024-03-01 09:35:04 +0000

    LRDOCS-12534 extra try block removed

--- docs/dxp/latest/en/installation-and-upgrades/securing-liferay/developer-guide/captcha-api-basics/resources/liferay-p6s7.zip/java/Captcha_GET_FromInstance.java
+++ docs/dxp/latest/en/installation-and-upgrades/securing-liferay/developer-guide/captcha-api-basics/resources/liferay-p6s7.zip/java/Captcha_GET_FromInstance.java
@@ -11,30 +11,28 @@ public class Captcha_GET_FromInstance {
 	 * java -classpath .:* Captcha_GET_FromInstance
 	 */
 	public static void main(String[] args) throws Exception {
-		try {
-			CaptchaResource.Builder builder = CaptchaResource.builder();
+		CaptchaResource.Builder builder = CaptchaResource.builder();
 
-			CaptchaResource captchaResource = builder.authentication(
-				"test@liferay.com", "learn"
-			).build();
+		CaptchaResource captchaResource = builder.authentication(
+			"test@liferay.com", "learn"
+		).build();
 
-			Captcha captcha = captchaResource.getCaptchaChallenge();
+		Captcha captcha = captchaResource.getCaptchaChallenge();
 
-			System.out.println("Token: " + captcha.getToken());
+		System.out.println("Token: " + captcha.getToken());
 
-			byte[] imageBytes = Base64.getDecoder(
-			).decode(
-				captcha.getImage(
-				).split(
-					","
-				)[1]
-			);
+		byte[] imageBytes = Base64.getDecoder(
+		).decode(
+			captcha.getImage(
+			).split(
+				","
+			)[1]
+		);
 
-			try (FileOutputStream fileOutputStream = new FileOutputStream(
-					"captcha.png")) {
+		try (FileOutputStream fileOutputStream = new FileOutputStream(
+				"captcha.png")) {
 
-				fileOutputStream.write(imageBytes);
-			}
+			fileOutputStream.write(imageBytes);
 		}
 	}
 

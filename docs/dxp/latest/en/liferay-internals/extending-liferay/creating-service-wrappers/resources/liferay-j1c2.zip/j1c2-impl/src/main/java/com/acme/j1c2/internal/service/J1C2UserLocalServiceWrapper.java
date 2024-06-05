683455f0b2594aaaf52230044e4114dd7148bbdf
Brian Chan <brian.chan@liferay.com>
2024-06-05 10:23:38 -0300

    Auto SF

--- docs/dxp/latest/en/liferay-internals/extending-liferay/creating-service-wrappers/resources/liferay-j1c2.zip/j1c2-impl/src/main/java/com/acme/j1c2/internal/service/J1C2UserLocalServiceWrapper.java
+++ docs/dxp/latest/en/liferay-internals/extending-liferay/creating-service-wrappers/resources/liferay-j1c2.zip/j1c2-impl/src/main/java/com/acme/j1c2/internal/service/J1C2UserLocalServiceWrapper.java
@@ -4,7 +4,6 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceWrapper;
-import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.service.UserLocalServiceWrapper;
 
 import java.util.Map;
@@ -14,24 +13,25 @@ import org.osgi.service.component.annotations.Component;
 @Component(service = ServiceWrapper.class)
 public class J1C2UserLocalServiceWrapper extends UserLocalServiceWrapper {
 
-    @Override
-    public int authenticateByEmailAddress(
-            long companyId, String emailAddress, String password,
-            Map<String, String[]> headerMap, Map<String, String[]> parameterMap,
-            Map<String, Object> resultsMap)
-        throws PortalException {
+	@Override
+	public int authenticateByEmailAddress(
+			long companyId, String emailAddress, String password,
+			Map<String, String[]> headerMap, Map<String, String[]> parameterMap,
+			Map<String, Object> resultsMap)
+		throws PortalException {
 
-        if (_log.isInfoEnabled()) {
-            _log.info(
-                "Invoking #authenticateByEmailAddress(long, String, String, " +
-                    "Map, Map, Map)");
-        }
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Invoking #authenticateByEmailAddress(long, String, String, " +
+					"Map, Map, Map)");
+		}
 
-        return super.authenticateByEmailAddress(
-            companyId, emailAddress, password, headerMap, parameterMap,
-            resultsMap);
-    }
+		return super.authenticateByEmailAddress(
+			companyId, emailAddress, password, headerMap, parameterMap,
+			resultsMap);
+	}
 
-    private static final Log _log = LogFactoryUtil.getLog(
-        J1C2UserLocalServiceWrapper.class);
+	private static final Log _log = LogFactoryUtil.getLog(
+		J1C2UserLocalServiceWrapper.class);
+
 }

d8dd427affeb689ff997e725f09ff01cab936156
ericthmoritsuka <ericthmoritsuka@gmail.com>
2024-06-05 19:14:02 -0300

    LRDOCS-12389 solved merge conflict

--- docs/dxp/latest/en/liferay-development/objects/understanding-object-integrations/using-custom-object-apis/using-object-apis-with-base64-encoded-files/resources/liferay-p7x7.zip/curl/Able_DELETE_ById.sh
+++ docs/dxp/latest/en/liferay-development/objects/understanding-object-integrations/using-custom-object-apis/using-object-apis-with-base64-encoded-files/resources/liferay-p7x7.zip/curl/Able_DELETE_ById.sh
@@ -1,12 +1,5 @@
 curl \
-<<<<<<< HEAD
 	"http://localhost:8080/o/c/ables/${1}" \
 	--header "accept: application/json" \
 	--request "DELETE" \
 	--user "test@liferay.com:learn"
-=======
-   "http://localhost:8080/o/c/ables/${1}" \
-   --header "accept: application/json" \
-   --request "DELETE" \
-   --user "test@liferay.com:learn"
->>>>>>> 09e017d (LRDOCS-12389 using-object-apis-with-base64-formatted-files new article and resource files)
--- docs/dxp/latest/en/liferay-development/objects/understanding-object-integrations/using-custom-object-apis/using-object-apis-with-base64-encoded-files/resources/liferay-p7x7.zip/curl/Able_GET_FromCompany.sh
+++ docs/dxp/latest/en/liferay-development/objects/understanding-object-integrations/using-custom-object-apis/using-object-apis-with-base64-encoded-files/resources/liferay-p7x7.zip/curl/Able_GET_FromCompany.sh
@@ -1,8 +1,4 @@
 curl \
 	"http://localhost:8080/o/c/ables/" \
-<<<<<<< HEAD
 	--header "accept: application/json" \
-=======
-   --header "accept: application/json" \
->>>>>>> 09e017d (LRDOCS-12389 using-object-apis-with-base64-formatted-files new article and resource files)
 	--user "test@liferay.com:learn"

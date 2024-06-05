15319aa9fd389a26e57ecb03aaa1bde8f78a385e
ericthmoritsuka <ericthmoritsuka@gmail.com>
2024-06-05 09:58:49 -0300

    LRDOCS-12389 added resource files for using-object-apis-with-base64-encoded-files.md

---
+++ docs/dxp/latest/en/liferay-development/objects/understanding-object-integrations/using-custom-object-apis/using-object-apis-with-base64-encoded-files/resources/liferay-p7x7.zip/curl/Able_DELETE_ById.sh
@@ -0,0 +1,5 @@
+curl \
+	"http://localhost:8080/o/c/ables/${1}" \
+	--header "accept: application/json" \
+	--request "DELETE" \
+	--user "test@liferay.com:learn"
---
+++ docs/dxp/latest/en/liferay-development/objects/understanding-object-integrations/using-custom-object-apis/using-object-apis-with-base64-encoded-files/resources/liferay-p7x7.zip/curl/Able_GET_FromCompany.sh
@@ -0,0 +1,4 @@
+curl \
+	"http://localhost:8080/o/c/ables/" \
+	--header "accept: application/json" \
+	--user "test@liferay.com:learn"

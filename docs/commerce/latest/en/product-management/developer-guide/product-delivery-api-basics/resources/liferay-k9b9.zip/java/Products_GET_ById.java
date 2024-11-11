e3e5e1b5752d7e1967e57041d67f5d5a61854aa0
Aswin Rajeev <aswin.rajeev@liferay.com>
2024-11-11 13:42:20 +0000

    LRDOCS-14242 java examples

---
+++ docs/commerce/latest/en/product-management/developer-guide/product-delivery-api-basics/resources/liferay-k9b9.zip/java/Products_GET_ById.java
@@ -0,0 +1,20 @@
+import com.liferay.headless.commerce.delivery.catalog.client.resource.v1_0.ProductResource;
+
+public class Products_GET_ById {
+
+	/**
+	 * java -classpath .:* -DchannelId=1234 -DproductId=5678 Products_GET_ById
+	 */
+	public static void main(String[] args) throws Exception {
+		ProductResource.Builder builder = ProductResource.builder();
+
+		ProductResource productResource = builder.authentication(
+			"test@liferay.com", "learn"
+		).build();
+
+		System.out.println(
+			productResource.getChannelProduct(
+				Long.valueOf(System.getProperty("channelId")), Long.valueOf(System.getProperty("productId")), null));
+	}
+
+}
---
+++ docs/commerce/latest/en/product-management/developer-guide/product-delivery-api-basics/resources/liferay-k9b9.zip/java/Products_GET_FromChannels.java
@@ -0,0 +1,25 @@
+import com.liferay.headless.commerce.delivery.catalog.client.dto.v1_0.Product;
+import com.liferay.headless.commerce.delivery.catalog.client.pagination.Page;
+import com.liferay.headless.commerce.delivery.catalog.client.pagination.Pagination;
+import com.liferay.headless.commerce.delivery.catalog.client.resource.v1_0.ProductResource;
+
+public class Products_GET_FromChannels {
+
+	/**
+	 * java -classpath .:* -DchannelId=1234 Products_GET_FromChannels
+	 */
+
+   	public static void main(String[] args) throws Exception {
+		ProductResource.Builder builder = ProductResource.builder();
+
+		ProductResource productResource = builder.authentication(
+			"test@liferay.com", "learn"
+		).build();
+
+		Page<Product> page = productResource.getChannelProductsPage(
+			Long.valueOf(System.getProperty("channelId")), null, null, null, Pagination.of(1, 2), null);
+
+		System.out.println(page);
+	}
+
+}
